42cde304a48a0f9f997da7f6165705c429cc95bc
cadviewer <61973235+CADViewer@users.noreply.github.com>
2021-08-16 08:16:38 +0200

    update CV6.5.15g

--- content/redlines/api_650/test01.js
+++ content/redlines/api_650/test01.js
@@ -14,7 +14,7 @@ var cvjs_stickyNotesRedlines_Base = {
 						triangle_design: "none",
 						polypath_arrow: "none",
 						redline_text: "%EC%95%88%EB%85%95%ED%95%98%EC%84%B8%EC%9A%94%20%EC%84%B8%EA%B3%84",
-						redline_font_size: "10",
+						redline_font_size: "undefined",
 						fill_opacity: "0.01",
 						transform: "T674.8842533302417,528.9593804481287S3.5",
 						drawingRotation: 0
@@ -40,6 +40,27 @@ var cvjs_stickyNotesRedlines_Base = {
 						transform: "none",
 						drawingRotation: 0
 					}
+						,
+					3: {
+						node: "RED_3",
+						name: "redline3",
+						id: "3",
+						layer: "RedLineLayer",
+						group: "unassigned",
+						color: "#FF0000",
+						strokeWidth: "8",
+						fill: "#FF0000",
+						username: "Bob Smith",
+						userid: "user_1",
+						currentPage: 2,
+						triangle_design: "none",
+						polypath_arrow: "none",
+						redline_text: "none",
+						redline_font_size: "undefined",
+						fill_opacity: "0.1",
+						transform: "none",
+						drawingRotation: 0
+					}
 }
 
 function cvjs_setUpStickyNotesRedlines(paper){
@@ -50,13 +71,19 @@ vqRedlines.push(cItemRed1);
 
 cvjs_stickyNotesRedlines.push(cvjs_stickyNotesRedlines_Base[0]);
 
-var cItemRed2= paper.path("M529.038762802785,486.68532522277894h382.5801997894155v76.0932994056296h-382.5801997894155v-76.0932994056296 Z ").attr({stroke: "#FF0000", "stroke-width": "0.5", "fill": "#FF0000", "fill-opacity": "0.1"})
+var cItemRed2= paper.path("M529.038762802785,486.68532522277894h382.5801997894155v76.0932994056296h-382.5801997894155v-76.0932994056296 Z  ").attr({stroke: "#FF0000", "stroke-width": "0.5", "fill": "#FF0000", "fill-opacity": "0.1"})
 .data("node","RED_2");
 vqRedlines.push(cItemRed2);
 
 cvjs_stickyNotesRedlines.push(cvjs_stickyNotesRedlines_Base[1]);
 
- cvjs_redline=2; 
+var cItemRed3= paper.path("M530.5173140495868,1050.495867768595h381.495867768595v196.2768595041322h-381.495867768595v-196.2768595041322 Z ").attr({stroke: "#FF0000", "stroke-width": "8", "fill": "#FF0000", "fill-opacity": "0.1"})
+.data("node","RED_3");
+vqRedlines.push(cItemRed3);
+
+cvjs_stickyNotesRedlines.push(cvjs_stickyNotesRedlines_Base[3]);
+
+ cvjs_redline=3; 
 
  cvjs_stickynote=0; 
 
